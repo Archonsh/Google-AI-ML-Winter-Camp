684d04cbc56f0efca230e1678e33442d33883915
Archonsh <archonshen@gmail.com>
2019-01-15 01:40:11 +0800

    fix data_cleaning

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import re
+import os
 
-WORKING_FOLDER = "/Users/Archon/Downloads/"
+WORKING_FOLDER = os.curdir
 #####################################################
 # Put the DMSC.csv directly under WORKING_FOLDER
 #####################################################
 
 raw_data = pd.read_csv(WORKING_FOLDER + '/DMSC.csv')
 
-raw_data.drop_duplicates()
-raw_data.dropna()
-
-for i in range(10):
+for i in range(len(raw_data)):
     comment = raw_data.Comment[i]
     filtrate = re.compile(u'[^\u4E00-\u9FA5A-Za-z0-9_，。、:！,.!《》<> ]')
     filtrate_str = filtrate.sub(r'', comment)
     print(filtrate_str)
     raw_data.loc[i, 'Comment'] = filtrate_str
+
+raw_data.drop_duplicates()
+raw_data.dropna()
 
 raw_data.to_csv(WORKING_FOLDER + "/DMSC_cleaned.csv", encoding='utf-8')
