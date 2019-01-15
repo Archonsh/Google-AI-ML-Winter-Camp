b7e9cbf1e9f8aa3dadf990c4b277cf7707096d85
Archonsh <archonshen@gmail.com>
2019-01-15 14:28:32 +0800

    remove re (too slow) convert to simplified chinese

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 import os
+from opencc import OpenCC
 
 WORKING_FOLDER = os.curdir
 #####################################################
@@ -8,13 +9,14 @@ WORKING_FOLDER = os.curdir
 #####################################################
 
 raw_data = pd.read_csv(WORKING_FOLDER + '/DMSC.csv')
+cc = OpenCC('t2s')
 
 for i in range(len(raw_data)):
     comment = raw_data.Comment[i]
-    filtrate = re.compile(u'[^\u4E00-\u9FA5A-Za-z0-9_，。、:！,.!《》<> ]')
-    filtrate_str = filtrate.sub(r'', comment)
-    print(filtrate_str)
-    raw_data.loc[i, 'Comment'] = filtrate_str
+    # filtrate = re.compile(u'[^\u4E00-\u9FA5A-Za-z0-9_，。、:！,.!《》<>]')
+    # filtrate_str = filtrate.sub(r' ', comment)
+    raw_data.loc[i, 'Comment'] = cc.convert(comment)
+    print(i)
 
 raw_data.drop_duplicates()
 raw_data.dropna()
