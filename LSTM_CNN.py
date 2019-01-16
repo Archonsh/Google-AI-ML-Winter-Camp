f63f539e8571efdd99990fb11ae7b867db427f7e
Archonsh <archonshen@gmail.com>
2019-01-17 00:41:00 +0800

    tune CNN GRU epoch = 20 dr=0.3 lr=0.003

--- LSTM_CNN.py
+++ LSTM_CNN.py
@@ -17,7 +17,7 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
 WORKING_FOLDER = os.curdir
-EMBEDDING_NAME = 'sgns.zhihu.word'
+EMBEDDING_NAME = 'sgns.weibo.word'
 MAX_NB_WORDS = 500  # length of seq
 EMBEDDING_DIM = 300  # length of embedding
 MAX_NB_FEATURES = 500000  # max number of words in use
@@ -101,7 +101,7 @@ def build_model1(lr=0.0, lr_d=0.0, units=0, spatial_dr=0.0, kernel_size1=3, kern
     x = Dense(5, activation="sigmoid")(x)
     model = Model(inputs=inp, outputs=x)
     model.compile(loss="binary_crossentropy", optimizer=Adam(lr=lr, decay=lr_d), metrics=["accuracy"])
-    history = model.fit(X_train, y_ohe, batch_size=128, epochs=5, validation_split=0.1,
+    history = model.fit(X_train, y_ohe, batch_size=128, epochs=20, validation_split=0.1,
                         verbose=1, callbacks=[check_point, early_stop])
     model = load_model(file_path)
     return model
