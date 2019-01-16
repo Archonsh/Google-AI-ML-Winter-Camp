ee3bfdd5736f45414a1287929ba07bf3920cdfef
Archonsh <archonshen@gmail.com>
2019-01-16 21:31:30 +0800

    fix test

--- test_acc.py
+++ test_acc.py
@@ -49,9 +49,8 @@ print('One hot encoding complete')
 
 pred_model = load_model(MODEL_NAME + '.hdf5')
 pred = pred_model.predict(X_test, batch_size=384, verbose=1)
-print(y_ohe[5], argmax(pred[5]), test_df['Star'][5])
-wrong_indices = [i for i, v in enumerate(pred) if argmax(pred[i])+1
-                 != test_df['Star'][i]]
+#print(y_ohe[5], argmax(pred[5]), test_df['Star'][5])
+wrong_indices = [i for i, v in enumerate(pred) if argmax(pred[i]) != test_df['Star'][i]]
 
 
 acc = 1 - len(wrong_indices) / float(len(y_ohe))
