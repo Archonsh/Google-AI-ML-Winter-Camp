dde69f46c5dfc8cad69d6b9afc1bac72a5f716f7
Archonsh <archonshen@gmail.com>
2019-01-17 11:41:06 +0800

    add print model

---
+++ print_model.py
@@ -0,0 +1,17 @@
+from keras.models import Model, load_model
+from keras.utils import plot_model
+import os
+import sys
+
+WORKING_FOLDER = os.curdir
+
+########################################################################
+def print_model(MODEL_NAME):
+    print("Working folder %s" % WORKING_FOLDER)
+    pred_model = load_model(MODEL_NAME)
+    plot_model(pred_model, to_file=MODEL_NAME+'.png', dpi=300)
+    pred_model.summary()
+
+if __name__ == '__main__':
+    MODEL_NAME = sys.argv[1]
+    print_model(MODEL_NAME)
