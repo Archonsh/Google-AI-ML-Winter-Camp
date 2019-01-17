471c70b130b2dd7641aa96becf13dd70ce06d61d
Archonsh <archonshen@gmail.com>
2019-01-17 15:56:00 +0800

    output prediction to csv

--- test_acc.py
+++ test_acc.py
@@ -49,23 +49,31 @@ def test_acc(MODEL_NAME):
     pred = pred_model.predict(X_test, batch_size=384, verbose=1)
     plot_model(pred_model, to_file=MODEL_NAME+'.png')
 
-    # print(y_ohe[5], argmax(pred[5]), test_df['Star'][5])
     arg_rank = np.argsort(-pred, axis=1)
-    wrong_indices = [i for i, v in enumerate(pred) if arg_rank[i][0] != argmax(y_ohe[i])]
-    wrong_indices_2 = [i for i, v in enumerate(pred) if arg_rank[i][0] != argmax(y_ohe[i]) and arg_rank[i][1] != argmax(y_ohe[i])]
 
-    acc = 1 - len(wrong_indices) / float(len(y_ohe))
-    acc_2 = 1 - len(wrong_indices_2) / float(len(y_ohe))
-    print("Test accuracy: %f on %s" % (acc, MODEL_NAME))
-    print("Top 2 accuracy: %f" % acc_2)
-    print("------------------------------------------------")
-    print("------------------------------------------------")
-    print("Below is selections of wrongly predicted points:")
+    import csv
+    with open(MODEL_NAME[:-5]+'.csv', 'wb') as f:
+        writer = csv.writer(f)
+        for i in range(len(arg_rank)):
+            writer.writerow(arg_rank[i])
 
-    for i in wrong_indices[100:159]:
-        print("predicted: %d  real: %d" % (arg_rank[i][0] + 1, test_df['Star'][i]))
-    for i in wrong_indices[100:159]:
-        print(test_df['Comment'][i])
+    # wrong_indices = [i for i, v in enumerate(pred) if arg_rank[i][0] != argmax(y_ohe[i])]
+    # wrong_indices_2 = [i for i, v in enumerate(pred) if arg_rank[i][0] != argmax(y_ohe[i]) and arg_rank[i][1] != argmax(y_ohe[i])]
+    #
+    # acc = 1 - len(wrong_indices) / float(len(y_ohe))
+    # acc_2 = 1 - len(wrong_indices_2) / float(len(y_ohe))
+    # print("Test accuracy: %f on %s" % (acc, MODEL_NAME))
+    # print("Top 2 accuracy: %f" % acc_2)
+    # print("------------------------------------------------")
+    # print("------------------------------------------------")
+    # print("Below is selections of wrongly predicted points:")
+    #
+    # for i in wrong_indices[100:159]:
+    #     print("predicted: %d  real: %d" % (arg_rank[i][0] + 1, test_df['Star'][i]))
+    # for i in wrong_indices[100:159]:
+    #     print(test_df['Comment'][i])
+
+
 
 if __name__ == '__main__':
     MODEL_NAME = sys.argv[1]
