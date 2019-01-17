8295572d8966fc7f4c60f22d2dd34c4e67df0d46
Archonsh <archonshen@gmail.com>
2019-01-17 16:03:47 +0800

    output prediction to csv

--- output_prediction.py
+++ output_prediction.py
@@ -17,6 +17,7 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from numpy import argmax
 import sys
+import csv
 
 WORKING_FOLDER = os.curdir
 EMBEDDING_NAME = 'sgns.weibo.word'
@@ -25,7 +26,7 @@ EMBEDDING_DIM = 300  # length of embedding
 MAX_NB_FEATURES = 500000  # max number of words in use
 
 ########################################################################
-def test_acc(MODEL_NAME):
+def test_acc():
     print("Working folder %s" % WORKING_FOLDER)
 
     train_df = pd.read_csv(WORKING_FOLDER + '/DMSC_train.csv')
@@ -35,40 +36,23 @@ def test_acc(MODEL_NAME):
     tk = Tokenizer()
     tk.fit_on_texts(train_df['Comment'])
 
-    train_tokenized = tk.texts_to_sequences(train_df['Comment'])
     test_tokenized = tk.texts_to_sequences(test_df['Comment'])
     print("Tokenize complete")
 
     X_test = pad_sequences(test_tokenized, maxlen=MAX_NB_WORDS)
 
-    ohe = OneHotEncoder(sparse=False)
-    y_ohe = ohe.fit_transform(test_df['Star'].values.reshape(-1, 1))
-    print('One hot encoding complete')
+    M = ['CNN_GRU_20ep_MODEL.hdf5','CNN_ONLY_MODEL_SOFTMAX_L2_conv64.hdf5 ', 'LTSM_CNN_MODEL.hdf5', 'PARALLEL_LTSM_GRU_BEST_MODEL.hdf5']
 
-    pred_model = load_model(MODEL_NAME)
-    pred = pred_model.predict(X_test, batch_size=384, verbose=1)
-    plot_model(pred_model, to_file=MODEL_NAME+'.png')
+    for MODEL_NAME in M:
+        pred_model = load_model(MODEL_NAME)
+        pred = pred_model.predict(X_test, batch_size=384, verbose=1)
+        arg_rank = np.argsort(-pred, axis=1)
 
-    arg_rank = np.argsort(-pred, axis=1)
-
-    wrong_indices = [i for i, v in enumerate(pred) if arg_rank[i][0] != argmax(y_ohe[i])]
-    wrong_indices_2 = [i for i, v in enumerate(pred) if arg_rank[i][0] != argmax(y_ohe[i]) and arg_rank[i][1] != argmax(y_ohe[i])]
-
-    acc = 1 - len(wrong_indices) / float(len(y_ohe))
-    acc_2 = 1 - len(wrong_indices_2) / float(len(y_ohe))
-    print("Test accuracy: %f on %s" % (acc, MODEL_NAME))
-    print("Top 2 accuracy: %f" % acc_2)
-    print("------------------------------------------------")
-    print("------------------------------------------------")
-    print("Below is selections of wrongly predicted points:")
-
-    for i in wrong_indices[100:159]:
-        print("predicted: %d  real: %d" % (arg_rank[i][0] + 1, test_df['Star'][i]))
-    for i in wrong_indices[100:159]:
-        print(test_df['Comment'][i])
-
+        with open('test.csv', 'w') as f:
+            wt = csv.writer(f)
+            for i in range(len(arg_rank)):
+                wt.writerow(arg_rank[i])
 
 
 if __name__ == '__main__':
-    MODEL_NAME = sys.argv[1]
-    test_acc(MODEL_NAME)
+    test_acc()
